aac954680f9896abf1c46167102d85a5532276e3
yangwei123 <348134071@qq.com>
2023-02-08 14:24:07 +0800

    init repo

---
+++ config/config_agent.go
@@ -0,0 +1,102 @@
+package config
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"sigs.k8s.io/yaml"
+
+	"github.com/opensourceways/server-common-lib/utils"
+)
+
+type Config interface {
+	Validate() error
+	SetDefault()
+}
+
+// The returned value of NewConfig must be a pointer.
+// Otherwise, it should deep copy the config when reading it.
+type NewConfig func() Config
+
+type ConfigAgent struct {
+	mut    sync.RWMutex
+	c      Config
+	b      NewConfig
+	md5Sum string
+	t      utils.Timer
+}
+
+func NewConfigAgent(b NewConfig) ConfigAgent {
+	return ConfigAgent{b: b, t: utils.NewTimer()}
+}
+
+func (ca *ConfigAgent) load(path string) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	content := []byte(os.ExpandEnv(string(b)))
+	md5Sum := fmt.Sprintf("%x", md5.Sum(content))
+	if ca.md5Sum == md5Sum {
+		return nil
+	}
+
+	c := ca.b()
+	if err := yaml.Unmarshal(content, c); err != nil {
+		return err
+	}
+
+	c.SetDefault()
+
+	if err := c.Validate(); err != nil {
+		return err
+	}
+
+	ca.mut.Lock()
+	ca.c = c
+	ca.md5Sum = md5Sum
+	ca.mut.Unlock()
+
+	return nil
+}
+
+func (ca *ConfigAgent) GetConfig() (string, Config) {
+	ca.mut.RLock()
+	c := ca.c // copy the pointer
+	v := ca.md5Sum
+	ca.mut.RUnlock()
+
+	return v, c
+}
+
+// Start starts polling path for plugin config.
+// If the first attempt fails, then start returns the error.
+func (ca *ConfigAgent) Start(path string) error {
+	if err := ca.load(path); err != nil {
+		return err
+	}
+
+	l := logrus.WithField("path", path)
+
+	ca.t.Start(
+		func() {
+			if err := ca.load(path); err != nil {
+				l.WithError(err).Error("loading config")
+			}
+		},
+		1*time.Minute,
+		0,
+	)
+
+	return nil
+}
+
+func (ca *ConfigAgent) Stop() {
+	ca.t.Stop()
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/opensourceways/server-common-lib
+
+go 1.18
+
+require (
+	github.com/sirupsen/logrus v1.9.0
+	k8s.io/apimachinery v0.26.1
+	sigs.k8s.io/yaml v1.3.0
+)
+
+require (
+	github.com/kr/pretty v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
