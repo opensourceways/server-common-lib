c9a6c883aa9fb19f61b42ac4f018455fd60e9cb4
yangwei999 <348134071@qq.com>
2023-08-31 11:34:00 +0800

    add gin server

---
+++ gin/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"github.com/opensourceways/server-common-lib/interrupts"
+)
+
+func StartWebServer(port int, timeout time.Duration, setRouter func(*gin.Engine)) {
+	r := gin.New()
+	r.Use(gin.Recovery())
+	r.Use(logRequest())
+
+	setRouter(r)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: r,
+	}
+
+	defer interrupts.WaitForGracefulShutdown()
+
+	interrupts.ListenAndServe(srv, timeout)
+}
+
+func logRequest() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		startTime := time.Now()
+
+		c.Next()
+
+		endTime := time.Now()
+
+		logrus.Infof(
+			"| %d | %d | %s | %s |",
+			c.Writer.Status(),
+			endTime.Sub(startTime),
+			c.Request.Method,
+			c.Request.RequestURI,
+		)
+	}
+}
